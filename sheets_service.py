4d42c23a48e766c3ef13e21266a8944a1816b9ff
Thulio Nicodemos <160731962+thulionicodemos@users.noreply.github.com>
2026-01-22 16:53:52 -0300

    Update credential handling in SheetsService
    Refactor SheetsService to use environment variable for credentials.

--- sheets_service.py
+++ sheets_service.py
@@ -1,5 +1,8 @@
+import os
+import json
 import gspread
 from google.oauth2.service_account import Credentials
+
 
 SCOPES = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -9,7 +12,15 @@ SCOPES = [
 
 class SheetsService:
     def __init__(self, credenciais_path: str, nome_planilha: str):
-        creds = Credentials.from_service_account_file(credenciais_path, scopes=SCOPES)
+        creds_json = os.getenv("GOOGLE_CREDENTIALS_JSON")
+
+        if not creds_json:
+            raise RuntimeError("Variável GOOGLE_CREDENTIALS_JSON não definida")
+
+        creds_dict = json.loads(creds_json)
+        
+        creds = Credentials.from_service_account_info(creds_dict, scopes=SCOPES)
+        
         self.client = gspread.authorize(creds)
         self.sheet = self.client.open(nome_planilha).worksheet("LANCAMENTOS_ONLINE")
 
